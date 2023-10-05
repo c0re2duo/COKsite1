9ed326289a9eb30a6c86a1a018b4d163b118eec0
c0re2duo <threeadripper@gmail.com>
2023-10-05 11:46:11 +0300

    Made button hint panel

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,27 @@
-// Функция для открытия модального окна
-	function openModal(text) {
-		var modal = document.getElementById("myModal");
-		var modal_content = modal.getElementsByClassName("modal-content");
-		var p = modal_content[0].getElementsByTagName("p")[0];
-		console.log(p);
-		p.innerHTML = text;
-		modal.style.display = "block";
-	}
+function openModal(text) {
+	var modal = document.getElementById("myModal");
+	var modal_content = modal.getElementsByClassName("modal-content");
+	var p = modal_content[0].getElementsByTagName("p")[0];
+	console.log(p);
+	p.innerHTML = text;
+	modal.style.display = "block";
+}
 
-	// Функция для закрытия модального окна
-	function closeModal(id) {
-		var modal = document.getElementById("myModal");
-		modal.style.display = "none";
-	}
+function closeModal(id) {
+	var modal = document.getElementById("myModal");
+	modal.style.display = "none";
+}
+
+
+function openButtonHint(hint) {
+	var button_hint_parent = document.getElementById("button_hint_parent");
+	button_hint_parent.style.display = "block";
+	var button_hint_p = document.getElementById('button_hint_p');
+	button_hint_p.innerHTML = hint;
+}
+
+
+function closeButtonHint() {
+	var button_hint_parent = document.getElementById("button_hint_parent");
+	button_hint_parent.style.display = "none"
+}
